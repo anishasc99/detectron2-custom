29004b8d25a0f9cb144e4ee13617c087e03ec68c
Anisha Chazhoor <67101520+anishasc99@users.noreply.github.com>
2021-05-03 17:11:53 +0530

    Add files via upload

---
+++ test/readme.txt
@@ -0,0 +1,3 @@
+folder to save image files that are being tested by test_one.py only
+annotations should be present to be tested by test_one.py.
+For test.py, follow the ipynb nb only.
---
+++ test_one.py
@@ -0,0 +1,92 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+from google.colab.patches import cv2_imshow
+import os
+import glob
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.data.catalog import DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.visualizer import ColorMode
+
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+register_coco_instances("my_dataset_test", {}, "/content/test/_annotations.coco.json", "/content/test")
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
+#cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
+#cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ("my_dataset_val",)
+
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
+#cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
+cfg.SOLVER.IMS_PER_BATCH = 4  #Number of training images per iteration/step
+cfg.SOLVER.BASE_LR = 0.001  #Learning rate
+
+cfg.SOLVER.WARMUP_ITERS = 1000
+cfg.SOLVER.MAX_ITER = 1500 #adjust up if val mAP is still rising, adjust down if overfit
+cfg.SOLVER.STEPS = (1000, 1500)
+cfg.SOLVER.GAMMA = 0.05  #The iteration number to decrease learning rate by GAMMA.
+
+
+   
+
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64 #RoI minibatch size *per image* (number of regions of interest [ROIs]), Total number of RoIs per training minibatch = ROI_HEADS.BATCH_SIZE_PER_IMAGE * SOLVER.IMS_PER_BATCH
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9 #your number of classes + 1
+
+cfg.TEST.EVAL_PERIOD = 500
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.DATASETS.TEST = ("my_dataset_test", )
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.25   # set the testing threshold for this model
+predictor = DefaultPredictor(cfg)
+test_metadata = MetadataCatalog.get("my_dataset_test")
+
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.DATASETS.TEST = ("my_dataset_test", )
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.25   # set the testing threshold for this model
+predictor = DefaultPredictor(cfg)
+test_metadata = MetadataCatalog.get("my_dataset_test")
+
+for imageName in glob.glob('/content/invoice-flask-API/detectron2-custom/test/*jpg'):
+  im = cv2.imread(imageName)
+  outputs = predictor(im)
+  output_pred_boxes = outputs["instances"].pred_boxes
+  output_pred_classes = outputs["instances"].pred_classes
+  classes = []
+  j = 0
+  for i in output_pred_classes.__iter__():
+    classes.append(i.cpu().numpy())
+    
+  for i in output_pred_boxes.__iter__() :
+    coords = i.cpu().numpy()
+    x1 = int(coords[0])
+    y1 = int(coords[1])
+    x2 = int(coords[2])
+    y2 = int(coords[3])
+    crop_img = im[y1:y2, x1:x2]
+    cv2.imwrite("./cropped images/croppedIm_"+str(classes[j])+".jpg",crop_img)
+    j += 1
+
+  v = Visualizer(im[:, :, ::-1],
+                metadata=test_metadata, 
+                scale=0.8
+                 )
+  out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+  cv2_imshow(out.get_image()[:, :, ::-1])
+  cv2.imwrite("predictions_detectron.jpg",out.get_image()[:, :, ::-1] )
